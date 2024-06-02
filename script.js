5d07c7fece782a1ce1707bc204150ad78c409496
Fabio <fabio.cire.156@gmail.com>
2024-06-02 16:39:17 +0200

    Data is now filterable, however, filtering more than once breaks the chart

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 let startDate = null;
 let endDate = null;
 let fullData = null;
+let filteredData = null;
+
+const times = ["0.00","0.10","0.20","0.30","0.40","0.50","1.00","1.10","1.20","1.30","1.40","1.50",
+               "2.00","2.10","2.20","2.30","2.40","2.50","3.00","3.10","3.20","3.30","3.40","3.50","4.00","4.10",
+               "4.20","4.30","4.40","4.50","5.00","5.10","5.20","5.30","5.40","5.50","6.00","6.10","6.20","6.30",
+               "6.40","6.50","7.00","7.10","7.20","7.30","7.40","7.50","8.00","8.10","8.20","8.30","8.40","8.50",
+               "9.00","9.10","9.20","9.30","9.40","9.50","10.00","10.10","10.20","10.30","10.40","10.50","11.00",
+               "11.10","11.20","11.30","11.40","11.50","12.00","12.10","12.20","12.30","12.40","12.50","13.00",
+               "13.10","13.20","13.30","13.40","13.50","14.00","14.10","14.20","14.30","14.40","14.50","15.00",
+               "15.10","15.20","15.30","15.40","15.50","16.00","16.10","16.20","16.30","16.40","16.50","17.00",
+               "17.10","17.20","17.30","17.40","17.50","18.00","18.10","18.20","18.30","18.40","18.50","19.00",
+               "19.10","19.20","19.30","19.40","19.50","20.00","20.10","20.20","20.30","20.40","20.50","21.00",
+               "21.10","21.20","21.30","21.40","21.50","22.00","22.10","22.20","22.30","22.40","22.50","23.00",
+               "23.10","23.20","23.30","23.40","23.50"]
 
 function readInput(event) {
     const file = event.target.files[0];
@@ -9,17 +23,30 @@ function readInput(event) {
         reader.onload = function(e) {
             const text = e.target.result;
             fullData = d3.csvParse(text);
+
+            fullData = fullData.filter(row => {
+                ok = true;
+                for (i = 0; i < times.length; i++) {
+                    if (row[times[i]] == null || row[times[i]] == "") ok = false;
+                    if (!ok) break;
+                }
+                return ok;
+            });
+            filteredData = fullData;
         }
         reader.readAsText(file);
+
+        document.getElementById('filter').disabled = false; 
+        document.getElementById('buildCharts').disabled = false; 
     }
 }
 
 document.getElementById('startDate').addEventListener('change', function(_) {
-    startDate = document.getElementById('startDate').value;
+    startDate = new Date(document.getElementById('startDate').value);
     console.log("Start date changed");
 });
 document.getElementById('endDate').addEventListener('change', function(_) {
-    endDate = document.getElementById('endDate').value;
+    endDate = new Date(document.getElementById('endDate').value);
     console.log("End date changed");
 });
 
@@ -27,13 +54,19 @@ document.getElementById('fileInput').addEventListener('change', function(event) 
     readInput(event);
 });
 
+document.getElementById('filter').addEventListener('click', function(_) {
+    filteredData = fullData.filter(d => {
+        const date = new Date(d.Date);
+        return date >= startDate && date <= endDate;
+    });
+    console.log("Filtered data now contains " + filteredData.length + " elements");
+});
+
 document.getElementById('buildCharts').addEventListener('click', function(_) {
     let actions = fullData.map(d => d.Action);
     actions = actions.filter(action => action != "");
     console.log("Read " + actions.length + " actions");
-    // cols_to_remove = ['Action', 'Code', 'Time (tens of minutes)', 'Drank water (ml)', 'Drank soda (ml)', 'Drank beer (ml)',
-    //       'Drank wine (ml)', 'Drank liquor (ml)', 'Lunch outside?', 'Dinner outside?', 'Had pizza (dinner)?',
-    //       'Shower?', 'Poop?', 'Emotion intensity description', 'Emotion intensity value']
+    
     fullData.forEach(d => {
         delete d["Action"];
         delete d["Code"];
@@ -57,19 +90,6 @@ document.getElementById('buildCharts').addEventListener('click', function(_) {
 });
 
 function getTens(data, actions) {
-    times = ["0.00","0.10","0.20","0.30","0.40","0.50","1.00","1.10","1.20","1.30","1.40","1.50",
-    "2.00","2.10","2.20","2.30","2.40","2.50","3.00","3.10","3.20","3.30","3.40","3.50","4.00","4.10",
-    "4.20","4.30","4.40","4.50","5.00","5.10","5.20","5.30","5.40","5.50","6.00","6.10","6.20","6.30",
-    "6.40","6.50","7.00","7.10","7.20","7.30","7.40","7.50","8.00","8.10","8.20","8.30","8.40","8.50",
-    "9.00","9.10","9.20","9.30","9.40","9.50","10.00","10.10","10.20","10.30","10.40","10.50","11.00",
-    "11.10","11.20","11.30","11.40","11.50","12.00","12.10","12.20","12.30","12.40","12.50","13.00",
-    "13.10","13.20","13.30","13.40","13.50","14.00","14.10","14.20","14.30","14.40","14.50","15.00",
-    "15.10","15.20","15.30","15.40","15.50","16.00","16.10","16.20","16.30","16.40","16.50","17.00",
-    "17.10","17.20","17.30","17.40","17.50","18.00","18.10","18.20","18.30","18.40","18.50","19.00",
-    "19.10","19.20","19.30","19.40","19.50","20.00","20.10","20.20","20.30","20.40","20.50","21.00",
-    "21.10","21.20","21.30","21.40","21.50","22.00","22.10","22.20","22.30","22.40","22.50","23.00",
-    "23.10","23.20","23.30","23.40","23.50"]
-
     tens = {}
 
     actions.forEach(a => {
